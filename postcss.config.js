fb521465cb37a4a0b8630beba6d58eb92e3fb442
Anoop-LogCat <anoopbenzier1997@outlook.com>
2022-07-02 20:37:49 +0530

    tailwind, material ui configuration

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const tailwindConfig = path.resolve(__dirname, './tailwind.config.js');
+
+module.exports = {
+  plugins: {
+    tailwindcss: { config: tailwindConfig },
+    autoprefixer: {},
+  },
+};
